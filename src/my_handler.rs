1b4837c37a1378d0eb7c6870aa769280a5718ba0
slme9364 <ishikazu1104@yahoo.co.jp>
2017-03-03 21:31:46 +0900

    [Modify] unuse unwrap in my_handler.rs

--- src/my_handler.rs
+++ src/my_handler.rs
@@ -6,23 +6,30 @@ use reply;
 
 pub struct MyHandler;
 
-//static CHANNEL: &'static str = "test";
-
 #[allow(unused_variables)]
 impl slack::EventHandler for MyHandler {
     fn on_event(&mut self,
                 cli: &mut slack::RtmClient,
                 event: Result<slack::Event, slack::Error>,
                 raw_json: &str) {
-        if raw_json.contains("\"user\":") && raw_json.contains("\"text\":") {
-
-            let json = Json::from_str(raw_json).unwrap();
-            let text_string = json.find("text").unwrap().to_string();
-            let text_data = text_string.as_str();
-
-            reply::reply_message(cli, text_data);
-        }
         println!("on_event(event: {:?}, raw_json: {:?})", event, raw_json);
+
+        if raw_json.contains("\"user\":") && raw_json.contains("\"text\":") {
+            let json = match Json::from_str(raw_json) {
+                Ok(val) => val,
+                Err(_) => return,
+            };
+
+            let text_data = match json.find("text") {
+                Some(val) => val,
+                None => return,
+            };
+
+            let text_string = text_data.to_string();
+            let text_str = text_string.as_str();
+
+            reply::reply_message(cli, text_str);
+        }
     }
 
     fn on_ping(&mut self, cli: &mut slack::RtmClient) {
