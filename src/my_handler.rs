c8cd830ef8a8755486364143349c38e7ff2f2161
slme9364 <ishikazu1104@yahoo.co.jp>
2017-03-15 20:01:51 -0700

    [Add] add comment in my_handler.rs

--- src/my_handler.rs
+++ src/my_handler.rs
@@ -8,23 +8,26 @@ pub struct MyHandler;
 
 #[allow(unused_variables)]
 impl slack::EventHandler for MyHandler {
+    //if user action in slack
     fn on_event(&mut self,
                 cli: &mut slack::RtmClient,
                 event: Result<slack::Event, slack::Error>,
                 raw_json: &str) {
         println!("on_event(event: {:?}, raw_json: {:?})", event, raw_json);
 
+        //event -> JSON which user and text data include pick up
         if raw_json.contains("\"user\":") && raw_json.contains("\"text\":") {
+            //raw_json(str) -> json(JSON)
             let json = match Json::from_str(raw_json) {
                 Ok(val) => val,
                 Err(_) => return,
             };
-
+            //text data in json pick up
             let text_data = match json.find("text") {
                 Some(val) => val,
                 None => return,
             };
-
+            //text data cast json data to str
             let text_string = text_data.to_string();
             let text_str = text_string.as_str();
 
@@ -32,14 +35,17 @@ impl slack::EventHandler for MyHandler {
         }
     }
 
+    //keep connect
     fn on_ping(&mut self, cli: &mut slack::RtmClient) {
         println!("on_ping");
     }
 
+    //close connect
     fn on_close(&mut self, cli: &mut slack::RtmClient) {
         println!("on_close");
     }
 
+    //start connect
     fn on_connect(&mut self, cli: &mut slack::RtmClient) {
         println!("on_connect");
     }
