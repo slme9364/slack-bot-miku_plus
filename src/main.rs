f65505a9367a856985dab89ff6a82df84e1ca111
slme9364 <ishikazu1104@yahoo.co.jp>
2017-03-16 12:06:57 +0900

    [Add] add comment in main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,16 @@ mod reply;
 mod my_handler;
 
 fn main() {
+    //set api key
     let api_key = env!("SLACK_MIKU_PLUS_API_TOKEN");
     println!("set api key:{}", api_key);
 
+    //make client
     let mut handler = my_handler::MyHandler;
     let mut cli = slack::RtmClient::new(&api_key);
     println!("create cli");
 
+    //Bot running
     let r = cli.login_and_run::<my_handler::MyHandler>(&mut handler);
     match r {
         Ok(_) => {}
