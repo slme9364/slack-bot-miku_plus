e37e65038d23590ba66c28c0402237548ae283a0
slme9364 <ishikazu1104@yahoo.co.jp>
2017-03-16 18:42:20 +0900

    [Add] if user send message in #test, reply in #test

--- src/my_handler.rs
+++ src/my_handler.rs
@@ -15,23 +15,26 @@ impl slack::EventHandler for MyHandler {
                 raw_json: &str) {
         println!("on_event(event: {:?}, raw_json: {:?})", event, raw_json);
 
-        //event -> JSON which user and text data include pick up
-        if raw_json.contains("\"user\":") && raw_json.contains("\"text\":") {
-            //raw_json(str) -> json(JSON)
-            let json = match Json::from_str(raw_json) {
-                Ok(val) => val,
-                Err(_) => return,
-            };
-            //text data in json pick up
-            let text_data = match json.find("text") {
-                Some(val) => val,
-                None => return,
-            };
-            //text data cast json data to str
-            let text_string = text_data.to_string();
-            let text_str = text_string.as_str();
+        //#test channel id -> C45M040DA
+        if raw_json.contains("\"channel\":\"C45M040DA\"") {
+            //event -> JSON which user and text data include pick up
+            if raw_json.contains("\"user\":") && raw_json.contains("\"text\":") {
+                //raw_json(str) -> json(JSON)
+                let json = match Json::from_str(raw_json) {
+                    Ok(val) => val,
+                    Err(_) => return,
+                };
+                //text data in json pick up
+                let text_data = match json.find("text") {
+                    Some(val) => val,
+                    None => return,
+                };
+                //text data cast json data to str
+                let text_string = text_data.to_string();
+                let text_str = text_string.as_str();
 
-            reply::reply_message(cli, text_str);
+                reply::reply_message(cli, text_str);
+            }
         }
     }
 
