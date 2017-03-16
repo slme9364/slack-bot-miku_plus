bc9f3bd11c31661f4f1329d0dfce3443b9f0ad2a
slme9364 <ishikazu1104@yahoo.co.jp>
2017-03-17 08:32:34 +0900

    [Modify] use const usize

--- src/reply.rs
+++ src/reply.rs
@@ -1,28 +1,29 @@
 extern crate slack;
 
 static CHANNEL: &'static str = "#test";
+const KIND_OF_REPLY_TEXT: usize = 8;
 
 pub fn reply_message(cli: &mut slack::RtmClient, element: &str) {
 
-    let contain_text: [&'static str; 8] = ["疲れ",
-                                           "つかれ",
-                                           "進捗",
-                                           "おはよ",
-                                           "たのし",
-                                           "man miku_plus",
-                                           "miku_plus --help",
-                                           "miku_plus"];
+    let contain_text: [&'static str; KIND_OF_REPLY_TEXT] = ["疲れ",
+                                                            "つかれ",
+                                                            "進捗",
+                                                            "おはよ",
+                                                            "たのし",
+                                                            "man miku_plus",
+                                                            "miku_plus --help",
+                                                            "miku_plus"];
 
-    let reply_text: [&'static str; 8] = ["お疲れ様です、マスター♪ ",
-                                         "お疲れ様です、マスター♪ ",
-                                         "進捗どうですか？",
-                                         "おはようございます〜\n今日も1日コーディング頑張りましょう！",
-                                         "マスターが楽しいなら、私も嬉しいです♪",
-                                         "対話型bot: miku_plusです\n気軽に話しかけてください♪ ",
-                                         "対話型bot, miku_plusです\n気軽に話しかけてください♪ ",
-                                         "マスター、お呼びでしょうか？"];
+    let reply_text: [&'static str; KIND_OF_REPLY_TEXT] = ["お疲れ様です、マスター♪ ",
+                                                          "お疲れ様です、マスター♪ ",
+                                                          "進捗どうですか？",
+                                                          "おはようございます〜\n今日も1日コーディング頑張りましょう！",
+                                                          "マスターが楽しいなら、私も嬉しいです♪",
+                                                          "対話型bot: miku_plusです\n気軽に話しかけてください♪ ",
+                                                          "対話型bot, miku_plusです\n気軽に話しかけてください♪ ",
+                                                          "マスター、お呼びでしょうか？"];
 
-    for i in 0..8 {
+    for i in 0..KIND_OF_REPLY_TEXT {
         if element.contains(contain_text[i]) {
             let _ = cli.send_message(CHANNEL, reply_text[i]);
             return;
