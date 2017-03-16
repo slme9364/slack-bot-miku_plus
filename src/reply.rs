b9efcb86d781772c2440860fa06d00e8f4d69c72
slme9364 <ishikazu1104@yahoo.co.jp>
2017-03-17 08:00:26 +0900

    [Modify] cleaning code (many use if -> use for)

--- src/reply.rs
+++ src/reply.rs
@@ -3,25 +3,29 @@ extern crate slack;
 static CHANNEL: &'static str = "#test";
 
 pub fn reply_message(cli: &mut slack::RtmClient, element: &str) {
-    if element.contains("疲れ") || element.contains("つかれ") {
-        let _ = cli.send_message(CHANNEL, "お疲れ様です、マスター♪");
-    }
-    if element.contains("進捗") {
-        let _ = cli.send_message(CHANNEL, "進捗どうですか？");
-    }
-    if element.contains("おはよ") {
-        let _ = cli.send_message(CHANNEL, "おはようございます〜\n今日も1日コーディング頑張りましょう！");
-    }
-    if element.contains("たのし") {
-        let _ =
-            cli.send_message(CHANNEL,
-                             "マスターが楽しいなら、私も嬉しいです♪");
-    }
-    if element.contains("man miku_plus") || element.contains("miku_plus --help") {
-        let _ =
-            cli.send_message(CHANNEL,
-                             "対話型bot, miku_plusです\n気軽に話かけてください♪");
-    } else if element.contains("miku_plus") {
-        let _ = cli.send_message(CHANNEL, "マスター、お呼びでしょうか？");
+
+    let contain_text: [&'static str; 8] = ["疲れ",
+                                           "つかれ",
+                                           "進捗",
+                                           "おはよ",
+                                           "たのし",
+                                           "man miku_plus",
+                                           "miku_plus --help",
+                                           "miku_plus"];
+
+    let reply_text: [&'static str; 8] = ["お疲れ様です、マスター♪ ",
+                                         "お疲れ様です、マスター♪ ",
+                                         "進捗どうですか？",
+                                         "おはようございます〜\n今日も1日コーディング頑張りましょう！",
+                                         "マスターが楽しいなら、私も嬉しいです♪",
+                                         "対話型bot: miku_plusです\n気軽に話しかけてください♪ ",
+                                         "対話型bot, miku_plusです\n気軽に話しかけてください♪ ",
+                                         "マスター、お呼びでしょうか？"];
+
+    for i in 0..8 {
+        if element.contains(contain_text[i]) {
+            let _ = cli.send_message(CHANNEL, reply_text[i]);
+            return;
+        }
     }
 }
