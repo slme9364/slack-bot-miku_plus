3d311429b8f95056a2b91dfffe473040af01d301
slme9364 <ishikazu1104@yahoo.co.jp>
2017-04-01 16:06:38 +0900

    [Modify] When conection refuse, reconnect after 1min

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ extern crate slack;
 mod reply;
 mod my_handler;
 
+use std::time::Duration;
+use std::thread;
+
 fn main() {
     //set api key
     let api_key = env!("SLACK_MIKU_PLUS_API_TOKEN");
@@ -14,10 +17,13 @@ fn main() {
     println!("create cli");
 
     //Bot running
-    let r = cli.login_and_run::<my_handler::MyHandler>(&mut handler);
-    match r {
-        Ok(_) => {}
-        Err(err) => println!("Error: {}", err),
+    loop {
+        let r = cli.login_and_run::<my_handler::MyHandler>(&mut handler);
+        match r {
+            Ok(_) => println!("ok"),
+            Err(err) => println!("Error: {}", err),
+        }
+        thread::sleep(Duration::from_millis(60000));
     }
 
     println!("{}", cli.get_name().unwrap());
